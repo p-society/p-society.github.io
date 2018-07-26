c3bdd44d7eeb9db06da446bc5a457749a0af95bf
debck <debasish.sahoo1998@gmail.com>
2018-07-26 22:30:38 +0530

    linted ruby codes

--- _plugins/medium_post_display.rb
+++ _plugins/medium_post_display.rb
@@ -1,20 +1,22 @@
 require 'feedjira'
 module Jekyll
+  # Get RSS feed to the site
   class MediumPostDisplay < Generator
     safe true
     priority :high
-def generate(site)
+    def generate(site)
       jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
       site.collections['external_feed'] = jekyll_coll
-Feedjira::Feed.fetch_and_parse("https://medium.com/feed/programming-society-gazette/").entries.each do |e|
-        title = e[:title]
-        content = e[:content]
-        guid = e[:url]
-        path = "./_external_feed/" + title + ".md"
-        path = site.in_source_dir(path)
-        doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        doc.data['title'] = title;
-        doc.data['feed_content'] = content;
+      display(jekyll_coll, site)
+    end
+
+    def display(jekyll_coll, site)
+      Feedjira::Feed.fetch_and_parse('https://medium.com/feed/programming-society-gazette/')
+                    .entries.each do |e|
+        path = site.in_source_dir('./_external_feed/' + e[:title] + '.md')
+        doc = Jekyll::Document.new(path, site: site, collection: jekyll_coll)
+        doc.data['title'] = e[:title]
+        doc.data['feed_content'] = e[:content]
         jekyll_coll.docs << doc
       end
     end
